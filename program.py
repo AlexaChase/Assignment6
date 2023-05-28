c617808b9e5f6ea73808bcb274bd64d1d5d65456
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-05-28 19:57:59 +0000

    new changes

--- program.py
+++ program.py
@@ -1,17 +1,21 @@
 import random
 def formula(n):
-    value= 1
+    value= 0
     for i in range(n):
         calculation= (-1) ** i /(2*i+1)
         value= value + calculation
-    return calculation*4
+    return value*4
 
-iterations= input("Please enter a number or select:")
+iterations= input("Please enter 'select' to let the program choose a number or enter 'number' to choose your own: ")
+if iterations == "number":
+    number= int(input("Please enter a number: "))
+    pi=formula(number)
+    print("The value of pi is:", pi)
 if iterations== "select":
     number= random.randint(1, 100)
     print("The value selected by the program is:", number)
-    pi=formula(iterations)
+    pi=formula(number)
     print(pi)
-if iterations== int():
-    pi=formula(iterations)
-    print("The value of pi is: ", pi)
+else:
+    print("Error, please try again")
+    
